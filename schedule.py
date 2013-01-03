24ef8ced2eabef38c2dd02a5d79119c7e2b1e9a7
George Yiu <yiu.george@gmail.com>
2013-01-03 01:25:36 -0800

    sort output on increasing ccn

--- schedule.py
+++ schedule.py
@@ -29,18 +29,17 @@ class myThread(threading.Thread):
 		remaining = str(int(limit) - int(enrolled))
 		self.data.append((self.ccn, enrolled, limit, waitList, waitListLimit))
 
+
 def main():
 	dept = sys.argv[1]
 	num = sys.argv[2]
-	threadLock = threading.Lock()
-	data = []
-	search_url = 'https://osoc.berkeley.edu/OSOC/osoc?y=0&p_term=SP&p_deptname=--+Choose+a+Department+Name+--&p_classif=--+Choose+a+Course+Classification+--&p_presuf=--+Choose+a+Course+Prefix%2fSuffix+--&p_course=' + num + '&p_dept=' + dept + '&x=0'
-	openPage = urllib.urlopen(search_url)
-	contents = openPage.read()
-	match = re.findall(r'input type=\"hidden\" name=\"_InField2\" value=\"([0-9]*)\"', contents)
+	class_url = 'https://osoc.berkeley.edu/OSOC/osoc?y=0&p_term=SP&p_deptname=--+Choose+a+Department+Name+--&p_classif=--+Choose+a+Course+Classification+--&p_presuf=--+Choose+a+Course+Prefix%2fSuffix+--&p_course=' + num + '&p_dept=' + dept + '&x=0'
+	contents = urllib.urlopen(class_url).read()
 
+	data = []
 	threads = []
 
+	match = re.findall(r'input type=\"hidden\" name=\"_InField2\" value=\"([0-9]*)\"', contents)
 	for ccn in match:
 		threads.append(myThread(ccn, data))
 		threads[-1].start()
@@ -48,6 +47,7 @@ def main():
 	for t in threads:
 		t.join()
 
+	data = sorted(data, key=lambda x: x[0])
 	five_columns = '{0:<8} {1:<8} {2:<8} {3:<8} {4:<8}'
 	print five_columns.format('CCN', 'Enrolled', 'Limit', 'Waitlist', 'Limit')
 	for ccn, enrolled, limit, waitList, waitListLimit in data:
