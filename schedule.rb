20cccfadcc40e61c4c2d40a3f93196549bd4e863
George Yiu <yiu.george@gmail.com>
2013-01-04 23:39:22 -0800

    get section information for ruby file

--- schedule.rb
+++ schedule.rb
@@ -1,10 +1,6 @@
 # encoding: utf-8
-
-
 require 'nokogiri'
 require 'open-uri'
-
-
 
 def schedule(ccn)
 	numbers = [0, 0, 0, 0]
@@ -17,9 +13,7 @@ def schedule(ccn)
 	enrolled, limit, wait_list, wait_limit = numbers
 	e = enrolled + '/' + limit
 	w = wait_list + '/' + wait_limit
-
-	puts e, w
-
+	return e, w
 end
 
 def main()
@@ -30,28 +24,29 @@ def main()
 	class_url = 'https://osoc.berkeley.edu/OSOC/osoc?y=0&p_term=SP&p_deptname=--+Choose+a+Department+Name+--&p_classif=--+Choose+a+Course+Classification+--&p_presuf=--+Choose+a+Course+Prefix%2fSuffix+--&p_course=' + num + '&p_dept=' + dept + '&x=0'
 	doc = open(class_url).read
 	ccn_search = Regexp.new(/input type="hidden" name="_InField2" value="([0-9]*)"/)
-	doc.scan(ccn_search).each do |line|
-		ccns << line
+	doc.scan(ccn_search).each do |ccn|
+		ccns << ccn[0]
 	end
 
-
-	#ccns[i][0] contains all of the ccn's
-	schedule(ccns[0][0])
-
-	field_search2 = Regexp.new(/>([^:<]+)/)
-	#this split line doesn't work
-	for line in doc.split('\n')
-		if line.match(/:&#160;/)
-			raw = line.scan(field_search2)
-			if raw.length == 1
-				raw << ''
-			end
-			data << raw[1]
+	field_search2 = 
+	doc.each_line do |line|
+		if line.include? ':&#160;'
+			raw = line.scan(Regexp.new(/>([^<&]+)/))
+			data << raw[1][0].strip
 		end
 	end
 
+	sections = []
+	data.each_slice(11) do |section_info|
+		sections << section_info
+	end
 
-
+	puts ['Section', 'Enrolled', 'Waitlist'].map{|x| x.ljust(10)}.join('')
+	sections.zip(ccns).each do |section, ccn|
+		name = section[0].split(' ')[-2,2].join(' ')
+		e, w = schedule(ccn)
+		puts [name, e, w].map{|x| x.ljust(10)}.join('')
+	end
 end
 
 if __FILE__ == 'schedule.rb'
